8d34c665ee48160761d414698dd53c7c6837d8b7
Martin Galese <martin@galese.net>
2024-03-22 15:35:45 -0400

    documentation

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
+# MindatGIS
+
+*TLDR;* 
+* I've created a GeoPackage of [Mindat.org](http://Mindat.org) data available for download in the Releases section.
+* There is a second version that merges Mindat data with USGS information on public lands.
+
+## Mindat opens up
+Mindat is an amazing resource for learning about minerals and, for the rock collector, an awesome way to check out possible locations where those minerals might be found.  But as amazing as it may be, the data has historically been stuck in Mindat, with aggressive anti-scraping and "keep out" signs attached.  Luckily, this seems to be changing! 
+
+With the publication of the [OpenMindat paper](https://rmets.onlinelibrary.wiley.com/doi/10.1002/gdj3.204) last May, new resources were invested into integrating Mindat information into the wider community. There is even an early start of a Python [API library](https://github.com/ChuBL/OpenMindat) and [some example code](https://github.com/ChuBL/How-to-Use-Mindat-API?tab=readme-ov-file) available!
+
+## GIS meet mineral data
+
+One of my personal goals for Mindat information has always been to put it on the map, literally.  Although there are small map embeds on the Mindat website, I really wanted to be able to load all of the localitiy information into QGIS or some other GIS package to develop the information.  
+
+So I've wrapped some of the API with my own interface code, worked around bugs in the current API, and coded up a script to take in Mindat location and geomaterial information and prepare a GeoPackage for use in GIS software! (I use QGIS, but should work in anything OGC standards compliant.) 
+
+## Mineral Data, Meet Public Access
+
+The great thing about open data is combining it in unexpected ways (without having to ask anyone for permission). The USGS is clearly a big believer in open data and makes tons of different products.  
+
+One (newer) offering includes GIS data for [conserved and public lands all over the United States](https://www.usgs.gov/programs/gap-analysis-project/science/pad-us-data-download).  This dataset is itself a compilation, apparently, of many different sources that describe Federal, State, and municipal lands.  While **it does not tell you if mineral collecting is allowed**, it does have information on whether the public is allowed onto the land in the first place!
+
+Along with `geopandas` and data from Mindat, that means that we can generate maps that not only show mineral localities from Mindat, but also that indicate whether you **might** be able to go to the location and check it out for youself! 
+
+>Of course, this information might be wrong in any number of ways and doesn't even try to cover local rules about mineral, so do your own checking and mind any posted signs. 
+But it is a great step towards figuring out new places to look for minerals to collect!
+
+## Pain points and cautionary notes
+
+Things aren't entirely awesome yet, but I have hope!
+
+* Despite [several](https://github.com/ChuBL/OpenMindat/issues/4) [queries](https://www.mindat.org/mesg-652986.html), there is still no actual license specified for the data. 
+  * Open and FAIR are repeated frequently, but sadly that seems to mean non-commercial and non-standard restrictions. 
+  * See, e.g., "We are creating a forked copy of the CC-BY-NC-SA 4.0 licence tailored specifically for these needs." from the [OpenMindat paper](https://rmets.onlinelibrary.wiley.com/doi/10.1002/gdj3.204)
+  
+* The API itself is limited and buggy.  
+  * There is no way, for example, to retrieve localities by geographic region. 
+  * Indeed, trying country-based filtering for the United States [doesn't work](https://www.mindat.org/mesg-650453.html).  
+  * Nor is it possible to get the minerals (geomaterials) available for a specific locality, despite documentation suggesting so. [https://github.com/ChuBL/OpenMindat/issues/3]
+* Data hygeine is questionable. There may be better quality GeoJSON artifacts [eventually](https://github.com/ChuBL/OpenMindat/issues/7), but the current information has some sloppy [NA markers](https://github.com/ChuBL/OpenMindat/issues/6) and lacks any sort of [coordinate reference system](https://datacarpentry.org/organization-geospatial/03-crs.html) to accompany the raw floating point latitude and longitude data.  I'm just assuming EPSG:4326/WGS84.
+  * This may just be unavoidable, given the venerable and non-GIS based data collection history, but who knows.
+* The "official" Python package for accessing the API is probably best skipped for now.  It doesn't do anything for throttling or sanity checking the data and is (currently)[https://github.com/ChuBL/OpenMindat/issues/5] limited to extracting to files for some reason.
+
 
 # Additional Requirements
 
-gdal
+If you want to run this code, you will need to provide your own MinDat API Key. Instructions on getting one are [here](https://www.mindat.org/a/how_to_get_my_mindat_api_key).
+
+When you have it, make a `.env` file with an entry for
+`MINDAT_API_KEY=xxxxx`
--- mindatGIS/mindat.py
+++ mindatGIS/mindat.py
@@ -9,6 +9,37 @@ Params = Dict[str, str]
 
 
 class MindatAPI:
+    """
+    A Python wrapper for the Mindat.org API.
+
+    This class provides methods to interact with the Mindat.org API and retrieve information about localities and geomaterials.
+
+    Attributes:
+        api_key (str): The API key for accessing the Mindat.org API.
+        session (LimiterSession): The session object for rate limiting requests.
+        base_url (str): The base URL of the Mindat.org API.
+
+    Methods:
+        request(url: str, params: Optional[Params] = None) -> Any:
+            Sends a GET request to the specified URL with optional parameters and returns the response.
+
+        get_paged_list(url: str, params: Optional[Params] = None) -> Iterator:
+            Retrieves a paged list of results from the specified URL and returns an iterator over the results.
+
+        get_locality(locality_id: int, params: Optional[Params] = None) -> Optional[Locality]:
+            Retrieves information about a specific locality by its ID and returns a Locality object.
+
+        get_localities(params: Optional[Params] = None) -> Iterable[Locality]:
+            Retrieves a list of localities and returns an iterable of Locality objects.
+
+        get_geomaterial(geomaterial_id: int, params: Optional[Params] = None) -> Any:
+            Retrieves information about a specific geomaterial by its ID and returns the response.
+
+        get_geomaterials(params: Optional[Params] = dict) -> Iterator:
+            Retrieves a paged list of geomaterials and returns an iterator over the results.
+
+    """
+
     def __init__(
         self,
         api_key: str,
@@ -20,6 +51,21 @@ class MindatAPI:
         self.base_url = base_url
 
     def request(self, url: str, params: Optional[Params] = None) -> Any:
+        """
+        Sends a GET request to the specified URL with optional parameters and returns the response.
+
+        Args:
+            url (str): The URL to send the request to.
+            params (Optional[Params]): Optional parameters to include in the request.
+
+        Returns:
+            Any: The decoded JSON response.
+
+        Raises:
+            Exception: If the request fails with a non-200 status code.
+            json.decoder.JSONDecodeError: If the JSON response cannot be decoded.
+
+        """
         logging.debug(f"Requesting {url}")
         headers = {"Authorization": "Token " + self.api_key}
         response = self.session.get(url, params=params, headers=headers)
@@ -36,7 +82,18 @@ class MindatAPI:
 
         return decoded
 
-    def get_paged_list(self, url: str, params: Optional[Params] = None):
+    def get_paged_list(self, url: str, params: Optional[Params] = None) -> Iterator:
+        """
+        Retrieves a paged list of results from the specified URL and returns an iterator over the results.
+
+        Args:
+            url (str): The URL to retrieve the paged list from.
+            params (Optional[Params]): Optional parameters to include in the request.
+
+        Yields:
+            Any: The results from each page of the paged list.
+
+        """
         response = self.request(url, params)
         yield from response["results"]
         while response["next"]:
@@ -46,18 +103,62 @@ class MindatAPI:
     def get_locality(
         self, locality_id: int, params: Optional[Params] = None
     ) -> Optional[Locality]:
+        """
+        Retrieves information about a specific locality by its ID and returns a Locality object.
+
+        Args:
+            locality_id (int): The ID of the locality to retrieve.
+            params (Optional[Params]): Optional parameters to include in the request.
+
+        Returns:
+            Optional[Locality]: The Locality object representing the requested locality, or None if not found.
+
+        """
         record = self.request(self.base_url + f"/localities/{locality_id}", params)
         if record:
             return Locality.from_dict(record)
 
     def get_localities(self, params: Optional[Params] = None) -> Iterable[Locality]:
+        """
+        Retrieves a list of localities and returns an iterable of Locality objects.
+
+        Args:
+            params (Optional[Params]): Optional parameters to include in the request.
+
+        Yields:
+            Locality: The Locality objects representing the retrieved localities.
+
+        """
         results = self.get_paged_list(self.base_url + "/localities", params)
         for result in results:
             yield Locality.from_dict(result)
 
-    def get_geomaterial(self, geomaterial_id: int, params: Optional[Params] = None):
+    def get_geomaterial(
+        self, geomaterial_id: int, params: Optional[Params] = None
+    ) -> Any:
+        """
+        Retrieves information about a specific geomaterial by its ID and returns the response.
+
+        Args:
+            geomaterial_id (int): The ID of the geomaterial to retrieve.
+            params (Optional[Params]): Optional parameters to include in the request.
+
+        Returns:
+            Any: The decoded JSON response.
+
+        """
         return self.request(self.base_url + f"/geomaterials/{geomaterial_id}", params)
 
-    def get_geomaterials(self, params: Optional[Params] = dict):
+    def get_geomaterials(self, params: Optional[Params] = dict) -> Iterator:
+        """
+        Retrieves a paged list of geomaterials and returns an iterator over the results.
+
+        Args:
+            params (Optional[Params]): Optional parameters to include in the request.
+
+        Yields:
+            Any: The results from each page of the paged list.
+
+        """
         params = params.update({"expand": "locality"})
         return self.get_paged_list(self.base_url + "/geomaterials", params)
